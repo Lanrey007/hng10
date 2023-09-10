6198b274aff9eefd4136021fc022730d76eb2265
lanrey007 <abdurrahmonbadmus.ab@gmail.com>
2023-09-10 10:16:11 +0100

    first

---
+++ first.php
@@ -0,0 +1,34 @@
+<?php
+header('Content-Type: application/json'); 
+
+$slack_name=$_GET['slack_name'];
+$track=$_GET['track'];
+
+$dateTime = new DateTime('now', new DateTimeZone('Africa/Lagos')); 
+$time=$dateTime->format("Y-m-d H:i:s");
+$day=$dateTime->format("l");
+
+if (isset($slack_name) && isset($track)){
+   $response=array(
+    'slack_name'=> $slack_name,
+    'track'=> $track,
+    'current_day'=> $day,
+    'utc_time'=> $time,
+    'github_file_url'=> $slack_name,
+    'github_repo_url'=> $slack_name,
+    'status_code'=> 200,
+    );
+echo json_encode($response); 
+}else{
+    $response=array(
+    'current_day'=> $day,
+    'utc_time'=> $time,
+    'github_file_url'=> $slack_name,
+    'github_repo_url'=> $slack_name,
+    'status_code'=> 404,
+    );
+echo json_encode($response);
+}
+
+
+?>
